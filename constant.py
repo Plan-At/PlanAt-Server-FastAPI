089bce1bd6208605932611ac7d863e335091fbe8
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-02-18 13:04:13 -0800

    manual reformat

--- constant.py
+++ constant.py
@@ -32,5 +32,68 @@ class MediaAssets:
     FAVICON = "https://cdn.jsdelivr.net/gh/Plan-At/static-image/2022/02/17/favicon.ico"
 
 class DummyData:
-    USER_PROFILE = {"id":"","profile_url":"","unique_name":"","display_name":"","picture":{"avatar":{"regular":"","full":""},"background":{"regular":"","full":""}},"status":{"current_status":"","until":"","default_status":""},"about":{"short_description":"","full_description":""},"contact":{"email":"","phone":""},"public_tags":[{"id":"","name":""}],"public_friends":[{"id":"","name":""}],"public_teams":[{"id":"","name":""}]}
-    USER_CALENDAR = {"id":"","username":"","calendar_entry":[{"object_id":"1","event_id":"1","owner":"me","visibility":"public","start":"Monday 9AM","end":"Monday 9PM","name":"work","description":"endless work","type":"work","tags":["work","mandatory","not fun"]},{"object_id":"2","event_id":"2","owner":"me","visibility":"private","start":"Monday 9PM","end":"Monday 11PM","name":"rest","description":"having fun","type":"work","tags":["gaming","fun"]},{"object_id":"3","event_id":"3","owner":"me","visibility":"public","start":"Tuesday 9AM","end":"Tuesday 9PM","name":"work","description":"endless work","type":"work","tags":["work","mandatory","not fun"]}]}
+    USER_PROFILE = {
+                    "id":"",
+                    "profile_url":"",
+                    "unique_name":"",
+                    "display_name":"",
+                    "picture":{
+                        "avatar":{
+                            "regular":"",
+                            "full":""},
+                        "background":{
+                            "regular":"",
+                            "full":""}},
+                    "status":{
+                        "current_status":"",
+                        "until":"",
+                        "default_status":""},
+                    "about":{
+                        "short_description":"",
+                        "full_description":""},
+                    "contact":{
+                        "email":"",
+                        "phone":""},
+                    "public_tags":[
+                        {"id":"","name":""}],
+                    "public_friends":[
+                        {"id":"","name":""}],
+                    "public_teams":[
+                        {"id":"","name":""}]}
+    USER_CALENDAR = {
+                    "id":"",
+                    "username":"",
+                    "calendar_entry":
+                        [{
+                            "object_id":"1",
+                            "event_id":"1",
+                            "owner":"me",
+                            "visibility":"public",
+                            "start":"Monday 9AM",
+                            "end":"Monday 9PM",
+                            "name":"work",
+                            "description":"endless work",
+                            "type":"work",
+                            "tags":["work","mandatory","not fun"]},
+                        {
+                            "object_id":"2",
+                            "event_id":"2",
+                            "owner":"me",
+                            "visibility":"private",
+                            "start":"Monday 9PM",
+                            "end":"Monday 11PM",
+                            "name":"rest",
+                            "description":"having fun",
+                            "type":"work",
+                            "tags":["gaming","fun"]},
+                        {
+                            "object_id":"3",
+                            "event_id":"3",
+                            "owner":"me",
+                            "visibility":"public",
+                            "start":"Tuesday 9AM",
+                            "end":"Tuesday 9PM",
+                            "name":"work",
+                            "description":"endless work",
+                            "type":"work",
+                            "tags":["work","mandatory","not fun"]}]}
