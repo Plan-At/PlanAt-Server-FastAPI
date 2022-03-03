b26d9748a331a2b76b4f65e8fd696a68fea1f87b
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-03-03 12:05:16 -0800

    sync

--- test/mongodb.py
+++ test/mongodb.py
@@ -1,4 +1,9 @@
 import pymongo
-import os
+import json
 
-print(os.path.exists(".env"))
+if __name__ == "__main__":
+    my_token = json.load(open("app.token.json"))
+    mongodb_client = pymongo.MongoClient(my_token["mongodb"]["driver_url"])
+
+    my_db = mongodb_client.list_database_names()
+    print(my_db)
