5818370213efec1a27333720d7d46ab836954834
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-03-10 13:09:54 -0800

    Update mongodb_data_api.py

--- util/mongodb_data_api.py
+++ util/mongodb_data_api.py
@@ -22,3 +22,55 @@ def find_one(target_db: str, target_collection: str, find_filter: dict):
     }
     response = requests.request("POST", url, headers=headers, data=payload)
     return json.loads(response.text)["document"]
+
+
+def delete_one(target_db: str, target_collection: str, find_filter: dict):
+    url = "https://data.mongodb-api.com/app/data-whsfw/endpoint/data/beta/action/deleteOne"
+    payload = json.dumps({
+        "dataSource": DB_CLUSTER,
+        "database": target_db,
+        "collection": target_collection,
+        "filter": find_filter
+    })
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": TOKEN["mongodb"]["data_api_key"]
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return json.loads(response.text)["document"]
+
+
+def insert_one(target_db: str, target_collection: str, document_body: dict):
+    url = "https://data.mongodb-api.com/app/data-whsfw/endpoint/data/beta/action/insertOne"
+    payload = json.dumps({
+        "dataSource": DB_CLUSTER,
+        "database": target_db,
+        "collection": target_collection,
+        "document": document_body
+    })
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": TOKEN["mongodb"]["data_api_key"]
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return json.loads(response.text)["document"]
+
+
+def replace_one(target_db: str, target_collection: str, find_filter: dict, document_body: dict):
+    url = "https://data.mongodb-api.com/app/data-whsfw/endpoint/data/beta/action/replaceOne"
+    payload = json.dumps({
+        "dataSource": DB_CLUSTER,
+        "database": target_db,
+        "collection": target_collection,
+        "filter": find_filter,
+        "replacement": document_body
+    })
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Request-Headers": "*",
+        "api-key": TOKEN["mongodb"]["data_api_key"]
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return json.loads(response.text)
