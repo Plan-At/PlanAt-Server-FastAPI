4c35a16b4e8c854c0cd5ae7af8f75f45295b626a
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-04-07 20:09:01 +0000

    fix CORS

--- server.py
+++ server.py
@@ -34,7 +34,6 @@ app.add_exception_handler(
     )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
     allow_credentials=True,
     allow_methods=["OPTIONS", "POST", "GET"],
     allow_headers=["*"],
