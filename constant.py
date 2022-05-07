f4a475bcc04fd5d4b4c410abdf7cd0c7c25e1d0a
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-05-07 20:02:02 +0000

    disable rate limiting

--- constant.py
+++ constant.py
@@ -29,26 +29,41 @@ class ContentLimit:
 
 
 class RateLimitConfig:
-    NO_COMPUTE = "2/second"
-    LESS_COMPUTE = "1/second"
-    SOME_COMPUTE = "1/3second"
-    MORE_COMPUTE = "1/5second"
-    INTENSE_COMPUTE = "1/10second"
-
-    MIN_DB = "2/3second"
-    SOME_DB = "2/5second"
-    MAX_DB = "2/15second"
-
-    MICRO_SIZE = "1/second"
-    SMALL_SIZE = "1/5second"
-    MID_SIZE = "1/30second"
-    BIG_SIZE = "1/minute"
-
-    LOW_SENSITIVITY = "1/10second"
-    MID_SENSITIVITY = "1/30second"
-    HIGH_SENSITIVITY = "1/minute"
-
-    BURST = "20/minute"
+    ENABLE_RL = False
+    if ENABLE_RL:
+        NO_COMPUTE = "2/second"
+        LESS_COMPUTE = "1/second"
+        SOME_COMPUTE = "1/3second"
+        MORE_COMPUTE = "1/5second"
+        INTENSE_COMPUTE = "1/10second"
+        MIN_DB = "2/3second"
+        SOME_DB = "2/5second"
+        MAX_DB = "2/15second"
+        MICRO_SIZE = "1/second"
+        SMALL_SIZE = "1/5second"
+        MID_SIZE = "1/30second"
+        BIG_SIZE = "1/minute"
+        LOW_SENSITIVITY = "1/10second"
+        MID_SENSITIVITY = "1/30second"
+        HIGH_SENSITIVITY = "1/minute"
+        BURST = "20/minute"
+    else:
+        NO_COMPUTE = "100/second"
+        LESS_COMPUTE = "100/second"
+        SOME_COMPUTE = "100/second"
+        MORE_COMPUTE = "100/second"
+        INTENSE_COMPUTE = "100/second"
+        MIN_DB = "100/second"
+        SOME_DB = "100/second"
+        MAX_DB = "100/second"
+        MICRO_SIZE = "100/second"
+        SMALL_SIZE = "100/second"
+        MID_SIZE = "10/second"
+        BIG_SIZE = "10/second"
+        LOW_SENSITIVITY = "100/second"
+        MID_SENSITIVITY = "100/second"
+        HIGH_SENSITIVITY = "100/second"
+        BURST = "1000/second"
 
 
 class MediaAssets:
