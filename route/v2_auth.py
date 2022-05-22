3bae0acec2b3acbf06d6bb7a8aee900d4b5bef4b
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-05-22 14:40:02 -0700

    WBS

--- route/v2_auth.py
+++ route/v2_auth.py
@@ -15,21 +15,21 @@ router = APIRouter()
 
 
 @router.post("/token/generate", tags=["V2"])
-async def v2_generate_auth_token(request: Request, name_and_password: json_body.PasswordLoginBody):
+async def v2_generate_auth_token(request: Request, cred: json_body.PasswordLoginBody):
     mongo_client = DocumentDB.get_client()
     db_client = mongo_client.get_database(DocumentDB.DB)
     credential_verify_query = DocumentDB.find_one(collection="LoginV1",
-                                                  find_filter={"person_id": name_and_password.person_id},
+                                                  find_filter={"person_id": cred.person_id},
                                                   db_client=db_client)
     print(credential_verify_query)
     # the hash string generated using hashlib is lowercase
     if (credential_verify_query is None) or not (
-            hashlib.sha512(name_and_password.password.encode("utf-8")).hexdigest() == credential_verify_query["password_hash"]):
+            hashlib.sha512(cred.password.encode("utf-8")).hexdigest() == credential_verify_query["password_hash"]):
         mongo_client.close()
         return JSONResponse(status_code=403,
                             content={"status": "not found or not match",
-                                     "person_id": name_and_password.person_id,
-                                     "password": name_and_password.password})
+                                     "person_id": cred.person_id,
+                                     "password": cred.password})
     while True:
         # Checking if the same token already being use
         # There is no do-while loop in Python
@@ -40,12 +40,12 @@ async def v2_generate_auth_token(request: Request, name_and_password: json_body.
         if current_checking_query is None:
             break
     create_at = int(datetime.now().timestamp())
-    expire_at = create_at + name_and_password.token_lifespan
+    expire_at = create_at + cred.token_lifespan
     token_record_query = DocumentDB.insert_one(
         collection="TokenV3",
         document_body={
             "structure_version": 3,
-            "person_id": name_and_password.person_id,
+            "person_id": cred.person_id,
             "token_value": generated_token,
             "token_hash": hashlib.sha512(generated_token.encode("utf-8")).hexdigest(),
             "creation_timestamp_int": create_at,
@@ -79,36 +79,36 @@ async def v2_revoke_auth_token(request: Request, pa_token: str = Header(None)):
 
 # TODO: revoke existing session/token
 @router.post("/password/update", tags=["V2"])
-async def v2_update_auth_password(request: Request, old_password: json_body.PasswordLoginBody, new_password: json_body.PasswordLoginBody):
+async def v2_update_auth_password(request: Request, old_cred: json_body.PasswordLoginBody, new_cred: json_body.PasswordLoginBody):
     mongo_client = DocumentDB.get_client()
     db_client = mongo_client.get_database(DocumentDB.DB)
     credential_verify_query = DocumentDB.find_one(collection="LoginV1",
-                                                  find_filter={"person_id": old_password.person_id},
+                                                  find_filter={"person_id": old_cred.person_id},
                                                   db_client=db_client)
     print(credential_verify_query)
     # verify the old password
     if (credential_verify_query is None) or not (
-            hashlib.sha512(old_password.password.encode("utf-8")).hexdigest() == credential_verify_query["password_hash"]):
+            hashlib.sha512(old_cred.password.encode("utf-8")).hexdigest() == credential_verify_query["password_hash"]):
         mongo_client.close()
         return JSONResponse(status_code=403,
                             content={"status": "not found or not match",
-                                     "person_id": old_password.person_id,
-                                     "password": old_password.password})
+                                     "person_id": old_cred.person_id,
+                                     "password": old_cred.password})
     new_credential_entry = {
         "structure_version": 1,
-        "person_id": old_password.person_id,
-        "password_hash": hashlib.sha512(new_password.password.encode("utf-8")).hexdigest(),
-        "password_length": len(new_password.password),
+        "person_id": old_cred.person_id,
+        "password_hash": hashlib.sha512(new_cred.password.encode("utf-8")).hexdigest(),
+        "password_length": len(new_cred.password),
     }
     credential_update_query = DocumentDB.replace_one(collection="LoginV1",
-                                                     find_filter={"person_id": old_password.person_id},
+                                                     find_filter={"person_id": old_cred.person_id},
                                                      document_body=new_credential_entry,
                                                      db_client=db_client)
     print(credential_update_query)
     if credential_update_query.matched_count != 1 and credential_update_query.modified_count != 1:
         # trying to make the break in the first place, if no error might proceed to other steps
         return JSONResponse(status_code=500, content={"status": "failed to update",
-                                                      "person_id": old_password.person_id,
-                                                      "password": old_password.password})
+                                                      "person_id": old_cred.person_id,
+                                                      "password": old_cred.password})
     mongo_client.close()
-    return JSONResponse(status_code=200, content={"status": "success", "voided": old_password.password})
+    return JSONResponse(status_code=200, content={"status": "success", "voided": old_cred.password})
